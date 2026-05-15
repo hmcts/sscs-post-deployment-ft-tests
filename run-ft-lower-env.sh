f0d5640123d88f0ef72a062e64e1cee6a8e31ff0
Mike Brown <michael.brown@hmcts.net>
2026-05-15 11:42:41 +0100

    sscs vault removed from build.gradle, sscs replaced with probate in run-ft-lower-env.sh

--- run-ft-lower-env.sh
+++ run-ft-lower-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Use this script if you would like to run your sscs-post-deployment-ft-tests against a lower environment except preview.
+# Use this script if you would like to run your probate-post-deployment-ft-tests against a lower environment except preview.
 # Note the tests rely on the correct DMN files being deployed in the target env.
 #
 # In most cases it is preferred to use the script: ./run-ft-preview.sh
@@ -34,8 +34,8 @@ esac
 export FT_ENVIRONMENT="$1"
 export WA_POST_DEPLOYMENT_TEST_FT_ENVIRONMENT=${FT_ENVIRONMENT}
 export AZURE_SERVICE_BUS_TOPIC_NAME=ccd-case-events
-export AZURE_SERVICE_BUS_MESSAGE_AUTHOR=sscs-${FT_ENVIRONMENT}
-export SSCS_VAULT_NAME=sscs-${FT_ENVIRONMENT}
+export AZURE_SERVICE_BUS_MESSAGE_AUTHOR=probate-${FT_ENVIRONMENT}
+export PROBATE_VAULT_NAME=probate-${FT_ENVIRONMENT}
 export WA_VAULT_NAME=wa-${FT_ENVIRONMENT}
 export AZURE_SERVICE_BUS_SUBSCRIPTION_NAME=ccd-case-events-ft
 export S2S_NAME_TASK_MANAGEMENT_API=wa_task_management_api
@@ -65,8 +65,8 @@ loadSecret "WA_SYSTEM_USERNAME" ${WA_VAULT_NAME} "wa-system-username"
 loadSecret "WA_SYSTEM_PASSWORD" ${WA_VAULT_NAME} "wa-system-password"
 loadSecret "S2S_SECRET_TASK_MANAGEMENT_API" ${WA_VAULT_NAME} "s2s-secret-task-management-api"
 
-loadSecret "AZURE_SERVICE_BUS_CONNECTION_STRING" ${SSCS_VAULT_NAME} "sscs-servicebus-connection-string-tf"
-loadSecret "SYSTEMUPDATE_USERNAME" ${SSCS_VAULT_NAME} "idam-sscs-systemupdate-user"
-loadSecret "SYSTEMUPDATE_PASSWORD" ${SSCS_VAULT_NAME} "idam-sscs-systemupdate-password"
+loadSecret "AZURE_SERVICE_BUS_CONNECTION_STRING" ${PROBATE_VAULT_NAME} "probate-servicebus-connection-string-tf"
+loadSecret "SYSTEMUPDATE_USERNAME" ${PROBATE_VAULT_NAME} "idam-probate-systemupdate-user"
+loadSecret "SYSTEMUPDATE_PASSWORD" ${PROBATE_VAULT_NAME} "idam-probate-systemupdate-password"
 
 ./gradlew functional --tests ScenarioRunnerTest --info
